55fd4833644cd5e86e1e1de746fa75965abb29df
MatDegs <m.degas@gmail.com>
2024-12-01 23:23:46 +0100

    test chgt home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
             <Navigation />
             <h1>Accueil</h1>
             <p>Le message d'accueil</p>
-            <p className='text-blue-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, alias voluptatem placeat inventore architecto sit numquam ab maiores officia iste nam aspernatur quod omnis culpa corporis nostrum temporibus. Ut, eos nihil sequi magnam nisi sit aspernatur, doloremque ullam molestias, consectetur sapiente nesciunt modi hic minima alias aut. Debitis, quas asperiores!</p>
+            <p className='text-blue-500'>Salut je viens de changer le texte</p>
         </div>
     );
 };
