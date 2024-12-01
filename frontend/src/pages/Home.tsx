afec48d59ddc9eb3e3b15a18c1d179678a01d31a
Khepten <nicolas.degas@gmail.com>
2024-12-01 23:32:18 +0100

    ajout texte pour test raton pull

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,6 +10,7 @@ const Home = () => {
             <h1>Accueil</h1>
             <p>Le message d'accueil</p>
             <p className='text-blue-500'>Salut je viens de changer le texte</p>
+            	<p>je rajoute du texte</p>
         </div>
     );
 };
